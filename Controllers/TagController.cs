616caaf875775a2fd195b649d58c794f11912af3
rizbons <rizbo.ns.bk@gmail.com>
2023-01-10 09:32:01 +0100

    create tag

--- Controllers/TagController.cs
+++ Controllers/TagController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using newsApi.Models;
+using newsApi.Services.TagService;
 
 namespace newsApi.Controllers
 {
@@ -7,5 +9,27 @@ namespace newsApi.Controllers
     [ApiController]
     public class TagController : ControllerBase
     {
+        private readonly ITagService _tagService;
+
+        public TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTag(Tag tag)
+        {
+            var serviceResponse = await _tagService.CreateTag(tag);
+            if (serviceResponse.Data == null)
+            {
+                return BadRequest(serviceResponse);
+            }
+            else if (serviceResponse.Success == false)
+            {
+                return BadRequest(serviceResponse);
+            }
+            return Ok(serviceResponse);
+        }
     }
+}
 }
