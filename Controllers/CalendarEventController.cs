2efe3c800b16ee636720ca146dc91b323d4af6aa
rizbons <rizbo.ns.bk@gmail.com>
2023-01-26 05:30:36 +0100

    cal model + controller

---
+++ Controllers/CalendarEventController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using newsApi.Models;
+
+namespace newsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarEventController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateCalendarEvent([FromBody] CalendarEvent calendarEvent)
+        {
+            Console.WriteLine(calendarEvent.Title);
+            return Ok();
+        }
+    }
+}
---
+++ Models/CalendarEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace newsApi.Models
+{
+    public class CalendarEvent
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string HtmlData { get; set; } = string.Empty;
+        public DateTime DateAndTime { get; set; }
+    }
+}
